44d9fed02cc7be1ff4d807c5b04c041d2812f3ba
kzamoiski <kirillzamoiski@gmail.com>
2022-02-03 10:32:36 +0300

    Refactor resource

--- task-backend/src/main/java/com/solbeg/taskbackend/resource/ProfileInfoResource.java
+++ task-backend/src/main/java/com/solbeg/taskbackend/resource/ProfileInfoResource.java
@@ -7,7 +7,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import static org.springframework.http.ResponseEntity.created;
 import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
@@ -20,8 +22,14 @@ public class ProfileInfoResource {
 
     @PostMapping
     @ResponseStatus(value = HttpStatus.CREATED)
-    public ProfileInfo save(@RequestBody final ProfileInfoRequest request) {
-        return service.save(request);
+    public ResponseEntity<ProfileInfo> save(@RequestBody final ProfileInfoRequest request) {
+
+        ProfileInfo profileInfo = service.save(request);
+
+        return created(UriComponentsBuilder
+                .fromPath("/api/profile-info/{id}")
+                .build(profileInfo.getId()))
+                .body(profileInfo);
     }
 
     @PutMapping(value = "/{id}")
